1f35709dfdf616f43c26e640fd86cd071ea82c63
Pavithra Nair <pavithranair1616@gmail.com>
2023-09-14 22:39:46 +0530

    commit

--- task-00/ubuntu.md
+++ task-00/ubuntu.md
@@ -1,1 +1,1 @@
-
+Downloaded Ubuntu successfully.There was some issue with my laptop but it was resolved soon.
--- task-02/hello world1.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter a number: "))
-primes = []
-for num in range(2, n + 1):
-    if all(num % i != 0 for i in range(2, num)):
-        primes.append(num)
-print("The list of primes are:", primes)
